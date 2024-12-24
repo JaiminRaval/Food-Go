5ef243673a93a74f69146b3ed4fc6e1a2c642b4d
JaiminRaval <siliconvalley.jaimin@gmail.com>
2024-12-24 23:01:01 +0530

    Initial Commit

---
+++ Food-GoUITests/Food_GoUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  Food_GoUITestsLaunchTests.swift
+//  Food-GoUITests
+//
+//  Created by Jaimin Raval on 24/12/24.
+//
+
+import XCTest
+
+final class Food_GoUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
