6df0c7842e6f86944aca53bc12d96342150cbdcb
JaiminRaval <siliconvalley.jaimin@gmail.com>
2025-05-02 23:58:59 +0530

    added initial screen

---
+++ Food-Go/Views/SearchView.swift
@@ -0,0 +1,18 @@
+//
+//  SearchView.swift
+//  Food-Go
+//
+//  Created by Jaimin Raval on 05/01/25.
+//
+
+import SwiftUI
+
+struct SearchView: View {
+    var body: some View {
+        Text("Hello, qwerty!")
+    }
+}
+
+#Preview {
+    SearchView()
+}
